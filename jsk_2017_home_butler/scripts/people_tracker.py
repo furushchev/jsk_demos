28c6372db9ac70a5a89f1128398f3c5e5615cbb3
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-12-12 21:34:57 +0900

    update people_tracker

--- jsk_2017_home_butler/scripts/people_tracker.py
+++ jsk_2017_home_butler/scripts/people_tracker.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 # Author: furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
 
+import angles
+from collections import defaultdict
 from jsk_topic_tools import ConnectionBasedTransport
 import message_filters as MF
 import numpy as np
 import rospy
+import tf.transformations as T
 import tf2_ros
 import tf2_geometry_msgs
 assert tf2_geometry_msgs
@@ -103,9 +106,12 @@ class PeopleTracker(ConnectionBasedTransport):
         super(PeopleTracker, self).__init__()
 
         self.fixed_frame_id = rospy.get_param("~fixed_frame_id", "odom_combined")
-        self.distance_threshold = rospy.get_param("~distance_threshold", 0.1)
-        self.timeout_threshold = rospy.get_param("~timeout_threshold", 3.0)
+        self.distance_threshold = rospy.get_param("~distance_threshold", 0.25)
+        self.timeout_threshold = rospy.get_param("~timeout_threshold", 5.0)
         self.face_recognition_threshold = rospy.get_param("~face_recognition_threshold", 4300.0)
+        self.face_pose_threshold = rospy.get_param("~face_pose_threshold", 0.2)
+        self.attention_score_threshold = rospy.get_param("~attention_score_threshold", 30.0)
+        self.attention_timeout_threshold = rospy.get_param("~attention_timeout_threshold", 3.0)
         self.unknown_name_prefix = rospy.get_param("~unknown_name_prefix", "unknown")
 
         self.tfl = tf2_ros.BufferClient("/tf2_buffer_server")
@@ -114,13 +120,16 @@ class PeopleTracker(ConnectionBasedTransport):
             rospy.logfatal("Error: tf buffer server not found")
             return
 
-        self.class_pub = self.advertise(
-            "~output", ClassificationResult, queue_size=1)
+        self.people_pub = self.advertise(
+            "~output/people", ClassificationResult, queue_size=1)
+        self.attention_pub = self.advertise(
+            "~output/attention", ClassificationResult, queue_size=1)
 
     def subscribe(self):
-        self.counter = 0
+        self.name_counter = defaultdict(int)
         self.people = dict()
         self.last_received = None
+        self.attentions = defaultdict(list)
 
         approximate_sync = rospy.get_param("~approximate_sync", False)
         queue_size = rospy.get_param("~queue_size", 100)
@@ -149,29 +158,36 @@ class PeopleTracker(ConnectionBasedTransport):
         rospy.loginfo("unsubscribed")
 
     def msg_callback(self, poses, faces):
-        # filter cached poses
-        if self.last_received and self.last_received < poses.header.stamp:
-            now = poses.header.stamp
+        # pop old poses
+        stamp = poses.header.stamp
+        if self.last_received and self.last_received < stamp:
             new_poses = dict()
             for n, p in self.people.items():
-                if (now - p.header.stamp).to_sec() < self.timeout_threshold:
+                if (stamp - p.header.stamp).to_sec() < self.timeout_threshold:
                     new_poses[n] = p
                 else:
                     rospy.logdebug("popped: %s" % n)
             self.people = new_poses
 
-        msg = ClassificationResult(header=poses.header)
-        msg.classifier = "people_tracker"
+        people_msg = ClassificationResult(header=poses.header)
+        people_msg.classifier = "people_tracker"
+
+        attention_msg = ClassificationResult(header=poses.header)
+        attention_msg.classifier = "attention_estimator"
 
         for pose, face in zip(poses.poses, faces.faces):
+            rot = pose.orientation
+            rot = T.euler_from_quaternion((rot.x, rot.y, rot.z, rot.w))
             pose = PoseStamped(header=poses.header, pose=pose)
 
+            # ensure transformation is from fixed frame
             if not pose.header.frame_id != self.fixed_frame_id:
                 try:
                     pose = self.tfl.transform(pose, self.fixed_frame_id)
                 except:
                     continue
 
+            # find near poses
             dups = list()
             for n, p in self.people.items():
                 d = pose_distance(pose, p)
@@ -179,6 +195,7 @@ class PeopleTracker(ConnectionBasedTransport):
                     dups.append((n, p))
                     del self.people[n]
 
+            # merge into one name
             if face.label:
                 name = face.label
                 proba = (self.face_recognition_threshold - face.confidence) / self.face_recognition_threshold
@@ -195,18 +212,42 @@ class PeopleTracker(ConnectionBasedTransport):
                 name = self.generate_name()
                 rospy.logdebug("None -> %s (new)" % name)
                 proba = 0.5
+
+            # fill class msg
             self.people[name] = pose
-            msg.label_names.append(name)
-            msg.label_proba.append(proba)
-        msg.target_names = list(set(msg.label_names))
+            people_msg.label_names.append(name)
+            people_msg.label_proba.append(proba)
 
-        self.class_pub.publish(msg)
-        self.last_received = poses.header.stamp
+            # count attention
+            rot_dist = angles.shortest_angular_distance(rot[0], -np.pi)
+            if rot_dist < self.face_pose_threshold:
+                self.attentions[name] += [stamp]
+            attentions = filter(
+                lambda t: (stamp-t).to_sec()<=self.attention_timeout_threshold,
+                sorted(self.attentions[name]))
+            self.attentions[name] = attentions
+            if len(attentions) < 2:
+                score = 0.0
+            else:
+                score = 1.0 * len(attentions) / self.attention_score_threshold
+                rospy.logdebug("attention: %f" % score)
+            score = min(1.0, max(0.0, score))
+            attention_msg.label_names.append(name)
+            attention_msg.label_proba.append(score)
 
-    def generate_name(self):
-        s = self.unknown_name_prefix + str(self.counter)
-        self.counter += 1
-        return s
+        people_msg.target_names = list(set(people_msg.label_names))
+        attention_msg.target_names = list(set(attention_msg.label_names))
+
+        self.people_pub.publish(people_msg)
+        self.attention_pub.publish(attention_msg)
+        self.last_received = stamp
+
+    def generate_name(self, base=None):
+        if base is None:
+            base = self.unknown_name_prefix
+        i = self.name_counter[base]
+        self.name_counter[base] += 1
+        return base + str(i)
 
 
 if __name__ == '__main__':
